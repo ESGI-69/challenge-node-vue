0269a91836975b948cb150cee6e780c165f40bfc
mrpinkcat <gatien.anizan@gmail.com>
2023-05-25 17:21:48 +0200

    remove: useless routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,12 +14,4 @@ app.use(securityRouter(userService));
 app.use('/users', userRouter);
 app.use('/users2', new GenericRouter(new GenericController(userService)));
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
-});
-
-app.post('/', (req, res) => {
-  res.json(req.body);
-});
-
 app.listen(3000, () => console.log('Server started on port 3000'));
