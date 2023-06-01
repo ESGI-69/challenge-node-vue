592aade1393b3d8f88bc5a6313083b0103195c71
mrpinkcat <gatien.anizan@gmail.com>
2023-06-01 13:41:40 +0200

    feat(seed): add user seeds

---
+++ backend/src/db/seeders/20230601110512-base-user.cjs
@@ -0,0 +1,46 @@
+'use strict';
+
+const bcrypt = require('bcryptjs');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('users', [
+      {
+        firstname: 'John',
+        lastname: 'Doe',
+        email: 'johndoe@example.com',
+        password: await bcrypt.hash('123456', 10),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        firstname: 'Jane',
+        lastname: 'Doe',
+        email: 'janedoe@example.com',
+        password: await bcrypt.hash('123456', 10),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down (queryInterface) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
