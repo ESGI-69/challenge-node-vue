28544e31a754590713077b751fab7ecacc6d14e9
timdev0 <quentin.peltier.pro@gmail.com>
2023-06-05 17:59:45 +0200

    feat(cards): connect store to api and display cards

--- frontend/src/stores/cardStore.js
+++ frontend/src/stores/cardStore.js
@@ -1,36 +1,25 @@
 import { defineStore } from 'pinia';
 
-// import $API from '@/plugins/axios';
+import $API from '@/plugins/axios';
 
 export const useCardStore = defineStore('cardStore', {
   state: () => ({
+    isCardLoading: false,
     cards: [],
     card: {},
   }),
 
   actions: {
     async getCards() {
-      // const response = await $API.get('/cards');
-      // this.cards = response.data;
-
-      // Fake data await
-      await new Promise((resolve) => setTimeout(resolve, 100));
-
-      // Fake data
-      this.cards = [
-        {
-          id: 1,
-          name: 'Card 1',
-          description: 'Description 1',
-          image: 'https://picsum.photos/3000',
-        },
-        {
-          id: 2,
-          name: 'Card 2',
-          description: 'Description 2',
-          image: 'https://picsum.photos/2000',
-        },
-      ];
+      this.isCardLoading = true;
+      try {
+        const { data } = await $API.get('/cards');
+        this.cards = data;
+      } catch (err) {
+        throw err.response.data;
+      } finally {
+        this.isCardLoading = false;
+      }
     },
   },
 });
---
+++ frontend/src/views/MyCards.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="my-cards">
+    <div
+      v-for="card in cards"
+      :key="card"
+    >
+      <card v-bind="card" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+import Card from '@/components/Card.vue';
+import { useCardStore } from '@/stores/cardStore';
+
+export default {
+  name: 'MyCards',
+  components: {
+    Card,
+  },
+  setup() {
+    const cardStore = useCardStore();
+
+    const cards = computed(() => cardStore.cards);
+
+    return {
+      cards,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.my-cards {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 1rem;
+}
+</style>
