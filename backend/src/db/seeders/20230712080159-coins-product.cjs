b6474386e586eabe58aec1eb3d88f28339436cdc
timdev0 <quentin.peltier.pro@gmail.com>
2023-07-12 11:18:14 +0200

    feat(product): create migration, model and seeder

---
+++ backend/src/db/seeders/20230712080159-coins-product.cjs
@@ -0,0 +1,43 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    // add coins product
+    await queryInterface.bulkInsert('products', [
+      {
+        name: '11000 Coins',
+        price: 10,
+        image: 'http://placehold.it/350x350',
+        categorie: 'COINS',
+        value: 11000,
+      },
+      {
+        name: '5000 Coins',
+        price: 5,
+        image: 'http://placehold.it/350x350',
+        categorie: 'COINS',
+        value: 5000,
+      },
+    ]);
+  },
+
+  async down (queryInterface) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('products', null, {});
+  },
+};
