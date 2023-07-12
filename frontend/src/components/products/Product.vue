29b8ad1b151f178a9c0bd7175205cb156b03a39d
timdev0 <quentin.peltier.pro@gmail.com>
2023-07-12 22:51:42 +0200

    feat(produts-front): WIP display products

---
+++ frontend/src/components/products/Product.vue
@@ -0,0 +1,50 @@
+<template>
+  <p>{{ name }}</p>
+  <p>{{ price }} €</p>
+  <p>{{ categorie }}</p>
+  <img 
+    v-if="categorie === 'COINS'"
+    :src="coinBag"
+    alt=""
+    width="160"
+    height="160"
+  >
+
+  <p>--------------------------------</p>
+</template>
+
+<script>
+import coinBag from '@/assets/coinBag.jpeg';
+export default {
+  name: 'Product',
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    categorie: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+  },
+  setup(props) {
+    return {
+      props,
+      coinBag,
+    };
+  },
+};
+
+</script>
