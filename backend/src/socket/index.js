c76f28576a68926d5d14cd70f420b7515a158039
mrpinkcat <gatien.anizan@gmail.com>
2023-07-12 17:39:52 +0200

    fix(SocketIo): reasign socket to room if the user reconnect

--- backend/src/socket/index.js
+++ backend/src/socket/index.js
@@ -1,5 +1,18 @@
 import { io } from '../index.js';
 import jwt from 'jsonwebtoken';
+import gameService from '../services/game.js';
+import userService from '../services/user.js';
+
+const reasignSocketToGameRoom = async (userId) => {
+  const user = await userService.findById(userId);
+  const game = await gameService.findByUser(user);
+  if (!user || !game) return;
+  const socket = users[user.id];
+  if (!socket) return;
+  await socket.join(game.id);
+  // eslint-disable-next-line no-console
+  console.log(`Socket ${socket.id} (user ${user.email}) reconnected to room ${game.id}`);
+};
 
 /**
  * @type {Object.<string, import('socket.io').Socket>}
@@ -15,6 +28,7 @@ export default () => {
       jwt.verify(clientJwt, process.env.JWT_SECRET);
       const { id } = jwt.decode(clientJwt);
       users[id] = client;
+      reasignSocketToGameRoom(id);
     } catch {
       console.error('Invalid jwt');
       client.disconnect();
